05898f10a98dbbe4e4c0a32cfe4061b83160717b
Shchedrovskyi Ivan <ltlaitoff@gmail.com>
2025-06-18 15:40:36 +0300

    feat: Rewrited entities

--- entities/entities.go
+++ entities/entities.go
@@ -1,42 +1,52 @@
 package entities
 
 import (
-	"go.mongodb.org/mongo-driver/v2/bson"
 	"time"
+
+	"github.com/ltlaitoff/2048/core"
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 type User struct {
 	ID        bson.ObjectID `bson:"_id,omitempty"`
 	Name      string        `bson:"name"`
+	Email     string        `bson:"email"`
 	CreatedAt time.Time     `bson:"created_at"`
 }
 
+type UserAuth struct {
+	ID           bson.ObjectID `bson:"_id,omitempty"`
+	UserID       bson.ObjectID `bson:"user_id"`
+	Email        string        `bson:"email"`
+	PasswordHash string        `bson:"password_hash"`
+	CreatedAt    time.Time     `bson:"created_at"`
+}
+
 type Session struct {
+	ID        bson.ObjectID `bson:"_id,omitempty"`
+	UserID    bson.ObjectID `bson:"user_id"`
+	CreatedAt time.Time     `bson:"created_at"`
+	ExpiredAt time.Time     `bson:"expired_at"`
+}
+
+type Run struct {
 	ID         bson.ObjectID `bson:"_id,omitempty"`
 	UserID     bson.ObjectID `bson:"user_id"`
 	Score      int           `bson:"score"`
-	Board      []int         `bson:"board"`
+	Board      core.Board    `bson:"board"`
 	UserAgent  string        `bson:"user_agent"`
 	CreatedAt  time.Time     `bson:"created_at"`
 	IsFinished bool          `bson:"is_finished,omitempty"`
-	FinishedAt *time.Time    `bson:"finished_at,omitempty"`
+	FinishedAt *time.Time     `bson:"finished_at,omitempty"`
+	Duration   int64         `bson:"duration_ms,omitempty"`
 }
 
-type SessionHistory struct {
-	ID               bson.ObjectID `bson:"_id,omitempty"`
-	SessionID        bson.ObjectID `bson:"session_id"`
-	Movement         MovementType  `bson:"movement"`
-	MovementDuration float64       `bson:"movement_duration_ms"`
-	CreatedAt        time.Time     `bson:"created_at"`
-}
-
-type Leaderboard struct {
-	ID         bson.ObjectID `bson:"_id,omitempty"`
-	UserID     bson.ObjectID `bson:"user_id"`
-	Score      int           `bson:"score"`
-	DurationMs float64       `bson:"duration_ms"`
-	UserAgent  string        `bson:"user_agent,omitempty"`
-	CreatedAt  time.Time     `bson:"created_at"`
+type RunHistoryRecord struct {
+	ID        bson.ObjectID `bson:"_id,omitempty"`
+	RunID     bson.ObjectID `bson:"run_id"`
+	Movement  MovementType  `bson:"movement"`
+	Duration  int64         `bson:"movement_duration_ms"`
+	CreatedAt time.Time     `bson:"created_at"`
 }
 
 type MovementType string
